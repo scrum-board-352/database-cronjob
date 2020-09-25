bf5d77293f1bdfd44bfac3ab10b7a04e48ddff33
jianglianEin <913057041@qq.com>
2020-09-25 19:08:00 +0800

    [jianglianEin] fixed ci.

--- script/prepare.sh
+++ script/prepare.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+if [ ! -x "~/database-cronjob"]; then
+  echo repo not exit.
+  git clone https://github.com/scrum-board-352/database-cronjob.git
+else
+  echo repo exit.
+  git pull
+fi
+
 pip3 install -r requirements.txt
