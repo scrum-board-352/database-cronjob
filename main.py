2aa21b4c8da23079841b4c4721a6bc95614d4438
jianglianEin <913057041@qq.com>
2020-09-26 00:30:57 +0800

    [jianglianEin] fixed python bug.

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ project_board_statement = "SELECT board_id, project_id from board_project_relati
 board_card_statement = "SELECT card_id, board_id from card_board_relation"
 comment_card_statement = "SELECT id, card_id from commit"
 
-host = os.getenv('host') if None else "127.0.0.1"
-postgres_user = os.getenv('postgres_user') if None else "postgres"
-postgres_pw = os.getenv('postgres_pw') if None else "postgres"
-project_postgres_port = os.getenv('project_postgres_port') if None else "5432"
-comment_postgres_port = os.getenv('comment_postgres_port') if None else "5432"
+host = os.getenv('host') if not None else "127.0.0.1"
+postgres_user = os.getenv('postgres_user') if not None else "postgres"
+postgres_pw = os.getenv('postgres_pw') if not None else "postgres"
+project_postgres_port = os.getenv('project_postgres_port') if not None else "5432"
+comment_postgres_port = os.getenv('comment_postgres_port') if not None else "5432"
 print("host: " + host + "postgres_user" + postgres_user + "postgres_pw" + postgres_pw)
 
 project_cur = connect_postgresql("scrumproject", postgres_user, postgres_pw, host, project_postgres_port)
@@ -21,7 +21,7 @@ board_card_dict = search_table_and_create_subtype_dict(project_cur, board_card_s
 comment_card_dict = search_table_and_create_subtype_dict(comment_cur, comment_card_statement)
 
 redis_pw = os.getenv('redis_pw')
-redis_port = os.getenv('redis_port') if None else "6379"
+redis_port = os.getenv('redis_port') if not None else "6379"
 
 client = connect_redis(host=host, port=redis_port, password=redis_pw)
 push_data_from_postgresql(project_board_dict, client, "project")
